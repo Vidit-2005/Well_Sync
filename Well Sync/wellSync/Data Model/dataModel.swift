7ec3250ff3081bae74b3f9b37f7f38cd3f5d718d
Kanak136 <gkanak136@gmail.com>
2026-02-10 10:04:56 +0530

    added Registration Screen

--- Well Sync/wellSync/Data Model/dataModel.swift
+++ Well Sync/wellSync/Data Model/dataModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct PsychologistDashboardStats: Codable {
+    var activePatientsCount: Int
+    var activePatientsDelta: Int
+    var todaysSessionsCount: Int
+}
+
+enum SessionStatus: String, Codable {
+    case upcoming
+    case completed
+    case missed
+}
+
+struct PatientSessionCard: Identifiable {
+    var id: UUID
+    var name: String
+    var profileImage: [String]
+    var disorder: String
+    var sessionCount: Int
+    var lastSessionDate: Date
+    var sessionTimeToday: String
+    var activityCompletionPercent: Int
+    //var statusIndicator: PatientStatus
+    var sessionStatus: SessionStatus
+}
+
